ef4aa03b9f48fb0428376bd4797b3fd260a278ba
yjhmelody <465402634@qq.com>
2017-05-06 23:51:08 +0800

    update files

---
+++ public/javascripts/form.js
@@ -0,0 +1,12 @@
+$('.form>button').click(function (e) {
+    var url = '/users';
+    var query = {};
+    $('.form>').each(function (index, elem) {
+        query[elem.name] = elem.value;
+    });
+
+    $.get(url, query, function (data) {
+        console.log(url, query, data);
+        
+    });
+});
