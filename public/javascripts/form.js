bcd10b20fc76efa8abce5b60bff313bf9c111981
yjhmelody <465402634@qq.com>
2017-05-18 22:28:43 +0800

    update

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -9,10 +9,13 @@ function formSearch(e) {
     var url = '/users';
     var query = {};
     $('.form>').each(function (index, elem) {
-        query[elem.name] = elem.value;
+        if(elem.name){
+            query[elem.name] = elem.value;
+        }
     });
 
     $.get(url, query, function (data) {
         console.log(url, query, data);
     });
+
 }
