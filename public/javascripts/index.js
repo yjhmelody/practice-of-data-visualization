a7fb72543d7b41806e66d84dedb805fdd2d341b8
yjhmelody <465402634@qq.com>
2017-05-18 19:45:05 +0800

    fix some error

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,5 +5,21 @@ window.addEventListener('load', function () {
     chart.init()
     // 加载时候获取所有点的坐标跟名称
     // url根据id返回经纬度
-    firstLoad('test/data.json')
+    new Promise((res, rej) => {
+            // 改成url
+            var url = 'test/data.json'
+            $.get(url).done((data) => {
+                res(data)
+            })
+        })
+        .then((data) => {
+            // 设置坐标点
+            map.setMarkers(data, 'click', (e) => {
+                // 给每个Marker设置相应的url
+                var url = 'test/data2.json'
+                $.get(url).done((data) => {
+                    chart.update(data)
+                })
+            })
+        })
 });
